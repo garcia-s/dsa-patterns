9b4228f2f3a51c43159b2d252fc954aa27b34902
Juan Garcia <garciajj18@gmail.com>
2023-08-10 17:38:23 -0500

    Added 2 implementations of jadencase

---
+++ Jadencase/jadencase.py
@@ -0,0 +1,22 @@
+tweet = "How can mirrors be real if our eyes aren't real"
+
+# Declaracion
+def jadencase(text):
+    #Separamos la frase en palabras
+    words = text.split(" ")
+    #Reemplazamos cada palabra con la version capitalizada
+    for i, word in enumerate(words):
+        words[i] = word.capitalize()
+    #Unimos las cadenas de texto y devolvemos el resultado
+    return " ".join(words)
+    
+def jadenCaseCopy(text):
+    cadena =""
+    for i,letra in enumerate(text):
+        if (i > 0 and text[i-1] == " "):
+            cadena += letra.upper()
+        else:
+            cadena += letra
+    return cadena
+
+print(jadenCaseCopy(tweet))
