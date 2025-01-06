5d7602b4feb8fd8ab700fe1c70db3a7e286d9ec7
Juan García <43949658+garcia-s@users.noreply.github.com>
2025-01-06 17:36:47 -0500

    NO, now we do structures.

---
+++ structures/rust/src/main.rs
@@ -0,0 +1,8 @@
+//This is just so the rust-analyzer doesn't yell at me
+
+// You have to include the files here because LSP is going to annoy you
+mod twosum {include!("twosum.rs");}
+
+fn main() {
+
+}
